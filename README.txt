524885c64240abb9df361744f4fce6a2b5697761
denis magno <denismagnocarvalho@gmail.com>
2025-11-23 14:54:35 -0300

    Versao final apresentada
    * projeto final corrijido com o gpsterco
    
    * variaveis char mudadas para unsigned char, problema de comparacao resolvido e subtracao de saldo dupla removido
    
    * erro de compilacao
    
    * Update README.txt
    
    * readme com instrucao extra adicionado
    
    * Acionado resistor de pull-up e modificado a direcao dos leds
    
    * correcao pra funcionamento do led de forma planejada

--- README.txt
+++ README.txt
@@ -1,5 +1,15 @@
-PC3 = led 2
-PC4 = led 1
-PC5 = btt
+PC3 = led de confirmacao
+PC4 = led de funcionamento
+PC5 = botao
 
 ddrc = 00011001
+
+3 sinais do led de funcionamento = cartao invalido
+2 sinais do led de funcinoamento = saldo invalido
+
+3 sinais do led de confirmacao = acesso permitido/cadastro feito com sucesso
+2 sinais do led de confirmacao = saldo depositado
+
+MODO DE CADASTRO:
+mantenha o botão pressionado e passe o cartão a ser cadastrado/depositar saldo,
+mantenha o botao pressionado até o led de funcionamento começar a piscar.
--- codigo.c
+++ codigo.c
@@ -15,39 +15,85 @@ Data Stack size : 256
 
 #include <stdio.h>
 #include <delay.h>
+#include <string.h>  
 #include "lib/rc522.h"
 #include "lib/rc522.c"
 
 #define STAND_BY 0 
 #define TRANSICAO_03 1 
-#define TRANSICAO_30 2 
+#define CONCLUSAO_2B 2 
 #define RECARGA 3 
-#define TRANSICAO_60 4 
+#define CONCLUSAO_3B 4 
 #define TRANSICAO_06 5 
 #define CONFIRMACAO 6 
+#define CARTAO_INVALIDO 7
+#define SALDO_INSUFICIENTE 8
+
+#define TAMANHO_DO_BANCO 10
+
+#define LED_FUNCIONAMENTO PORTC.4
+#define LED_CONFIRMACAO PORTC.3
 
 typedef struct {
     uint8_t card_id[5];
     uint16_t card_type;
-    uint16_t saldo;
-} BancoDeDados; //8 bit * 5 + 16 bit * 1 + 16 nit *1 = 9 bytes ~~ 11 byte
+    unsigned char saldo;
+} BancoDeDados; //8 bit * 5 + 16 bit * 1 + 8 bit *1 = 7 bytes ~~ 10 byte
 
-char ESTADO_DA_MAQUINA = STAND_BY; // 0 = stand-by, 1 = 0 para 3, 2 = 3 para 1, 3 = recarga, 4 = 6 para 1, 5 = 1 para 6, 6 = aguardo confirmacao 
+unsigned char estadoDaMaquina = STAND_BY; // 0 = stand-by, 1 = 0 para 3, 2 = 3 para 1, 3 = recarga, 4 = 6 para 1, 5 = 1 para 6, 6 = aguardo confirmacao 
 
-BancoDeDados bancoDeDados[10];
+BancoDeDados bancoDeDados[TAMANHO_DO_BANCO];
+unsigned char numero_cadastrado = 0;
+
+unsigned char consulta_conta_cadastrada(uint8_t *card_id, uint16_t card_type, unsigned char* posicao){
+    char i = 0;
+
+    for(i=0;i<numero_cadastrado;i++){
+        
+        if(card_type == bancoDeDados[i].card_type){
+
+            if(memcmp(card_id, bancoDeDados[i].card_id, 5) == 0){
+                *posicao = i;
+                return 1;
+            }
+
+        }
+    
+    }
+    *posicao = 11;
+    return 0;
+
+}
+
+unsigned char confirma_saldo(unsigned char posicao){
+    if(bancoDeDados[posicao].saldo == 0){
+        return 0;
+    }
+    return 1;
+}
+
+void subtrai_saldo(unsigned char posicao){
+    bancoDeDados[posicao].saldo--;
+}
 
 void main(void)
 {
     uint8_t card_id[5];
     uint16_t card_type;
-    char botao = 0; // not PINC.5
-    char leitura = 0; // 1 = pressionado
+    unsigned char botao = 0; // not PINC.5
+    unsigned char leitura = 0; // 1 = pressionado
+
+    unsigned char processa_cartao = 1;
+    
+    unsigned char posicao_da_conta = 0; // posicao da conta cadastrada
+
+    unsigned char timer_aux = 255;
 
     
     PORTB = 0x00;
     DDRB = 0x2C;
 
-    PORTC= 0x00;
+    PORTC= 0b00111000;
     DDRC= 0b00011001;
 
     PORTD=0x00;
@@ -102,41 +148,173 @@ void main(void)
     }
     while (1){
 
+        estadoDaMaquina = STAND_BY;
+        timer_aux = 0;
+        LED_CONFIRMACAO = 0;
+        LED_FUNCIONAMENTO = 0;
+        processa_cartao = 1;
+
         // botao = ~PINC.5;
         // leitura = rc522ReadCard(card_id, &card_type);
 
         if(rc522ReadCard(card_id, &card_type))
         {
-            printf("Card [0x%x, 0x%x, 0x%x, 0x%x, 0x%x]\n", card_id[0],card_id[1], card_id[2], card_id[3], card_id[4]);
+            
             /*
+                printf("Card [0x%x, 0x%x, 0x%x, 0x%x, 0x%x]\n", card_id[0],card_id[1], card_id[2], card_id[3], card_id[4]);
                 Importante fazer o print pelo menos uma fez pra saber o 
                 endereço da tag, caso desejem fazer algoexclusivo para ela.
             */
         
 
+            while(processa_cartao){
+                botao = ~PINC.5;
+                switch (estadoDaMaquina){
+                    case STAND_BY:
 
+                        if(botao){
+                            LED_FUNCIONAMENTO = 1;
+                            if(timer_aux>=100){
+                                estadoDaMaquina = TRANSICAO_03;
+                                timer_aux = 255;
+                                break;
+                            }
+                            break;
+                        }
+                        if(timer_aux>= 10){
+                            estadoDaMaquina = CARTAO_INVALIDO;
+                            timer_aux = 255;
+                            break;
+                        }
+
+                        LED_FUNCIONAMENTO = 0;
+                        timer_aux = 255;
+
+                        if( consulta_conta_cadastrada( card_id, card_type, &posicao_da_conta) ){
+                            if( confirma_saldo(posicao_da_conta)){
+                                    estadoDaMaquina = TRANSICAO_06;
+                                    break;
+                            }
+
+                            estadoDaMaquina = SALDO_INSUFICIENTE;
+                            break;
+                        }
+
+                        estadoDaMaquina = CARTAO_INVALIDO;
+                    break;    
+                    case TRANSICAO_03:
+                        if(botao == 0){
+                            estadoDaMaquina = RECARGA;
+                            LED_FUNCIONAMENTO = 0;
+                            timer_aux=255;
+                            break;
+                        }
+
+                        if(timer_aux >= 10){
+                            timer_aux = 0;
+                            LED_FUNCIONAMENTO = ~LED_FUNCIONAMENTO;
+                        }
+                    
+                    break;
+                    case CONCLUSAO_2B:
+                        LED_CONFIRMACAO = 1;
+                        delay_ms(200);
+                        LED_CONFIRMACAO = 0;
+                        delay_ms(100);
+                        LED_CONFIRMACAO = 1;
+                        delay_ms(200);
+                        LED_CONFIRMACAO = 0;
+                        processa_cartao = 0;
+                    break;
+                    case RECARGA:
+
+                        if( posicao_da_conta == 11 ){
+                            if(numero_cadastrado == 10){
+                                estadoDaMaquina = CARTAO_INVALIDO;
+                                break;
+                            }
+
+                            memcpy(bancoDeDados[numero_cadastrado].card_id, card_id, 5);
+                            bancoDeDados[numero_cadastrado].card_type =card_type; 
+                            bancoDeDados[numero_cadastrado].saldo = 5;
+                            
+                            numero_cadastrado ++;
+                            estadoDaMaquina = CONCLUSAO_3B;
+                            break;
+                        }
+
+                        bancoDeDados[posicao_da_conta].saldo +=5;
+                        estadoDaMaquina = CONCLUSAO_2B;
+
+                    break;
+                    case CONCLUSAO_3B:
+                        LED_CONFIRMACAO = 1;
+                        delay_ms(200);
+                        LED_CONFIRMACAO = 0;
+                        delay_ms(100);
+                        LED_CONFIRMACAO = 1;
+                        delay_ms(200);
+                        LED_CONFIRMACAO = 0;
+                        delay_ms(100);
+                        LED_CONFIRMACAO = 1;
+                        delay_ms(200);
+                        LED_CONFIRMACAO = 0;
+
+                        processa_cartao = 0;
+                    break;
+                    case TRANSICAO_06:
+                        LED_CONFIRMACAO = 1;
+                        delay_ms(200);
+                        LED_CONFIRMACAO = 0;
+                        
+                        estadoDaMaquina = CONFIRMACAO;
+                    break;
+                    case CONFIRMACAO:
+
+                        if(botao){
+                            subtrai_saldo(posicao_da_conta);
+                            estadoDaMaquina = CONCLUSAO_3B;
+                            break;
+                        }
+
+                        if(timer_aux >= 100){
+                            estadoDaMaquina = SALDO_INSUFICIENTE;
+                        }
+
+
+                    break;
+                    case CARTAO_INVALIDO:
+                        LED_FUNCIONAMENTO = 1;
+                        delay_ms(200);
+                        LED_FUNCIONAMENTO = 0;
+                        delay_ms(100);
+                        LED_FUNCIONAMENTO = 1;
+                        delay_ms(200);
+                        LED_FUNCIONAMENTO = 0;
+                        delay_ms(100);
+                        LED_FUNCIONAMENTO = 1;
+                        delay_ms(200);
+                        LED_FUNCIONAMENTO = 0;
+
+                        processa_cartao = 0;
+                    break;
+                    case SALDO_INSUFICIENTE:
+                        LED_FUNCIONAMENTO = 1;
+                        delay_ms(200);
+                        LED_FUNCIONAMENTO = 0;
+                        delay_ms(100);
+                        LED_FUNCIONAMENTO = 1;
+                        delay_ms(200);
+                        LED_FUNCIONAMENTO = 0;
+
+                        processa_cartao = 0;
+                    break;
+                }
+                timer_aux++;
+                delay_ms(50);
+            }
             delay_ms(1000);
         }
 
-        // switch (ESTADO_DA_MAQUINA){
-        //     case STAND_BY:
-        //         if(leitura){
-        //             printf("Card [0x%x, 0x%x, 0x%x, 0x%x, 0x%x]\n", card_id[0],card_id[1], card_id[2], card_id[3], card_id[4]);
-        //         }
-        //     break;
-        //     case TRANSICAO_03:
-        //     break;
-        //     case TRANSICAO_30:
-        //     break;
-        //     case RECARGA:
-        //     break;
-        //     case TRANSICAO_60:
-        //     break;
-        //     case TRANSICAO_06:
-        //     break;
-        //     case CONFIRMACAO:
-        //     break;
-        // }
-        // delay_ms(20);
     }
 }
